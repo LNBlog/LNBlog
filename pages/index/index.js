97b040d5790e7a563b1dab24117194bd382628cc
qiaoguangqing <2764800087@qq.com>
2020-01-31 12:43:08 +0800

    tabBar

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,102 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+// pages/log/log.js
+import formatTime from "./../../utils/util.js"
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+ 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const that = this;
+    wx.request({
+      url: 'http://139.224.25.165:8066/api/getPictureList',
+      data: {
+        pageNum: 1,
+        pageSize: 6
+      },
+      header: {
+        'content-type': 'application/json;charset=utf-8' // 默认值 
+      },
+      success(res) {
+        that.setData({
+          banners: res.data.result.list
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+    }),
+
+      wx.request({
+      url: 'http://139.224.25.165:8066/api/getNewsList',
+        data: {
+          pageNum: 1,
+          pageSize: 6
+        },
+        header: {
+          'content-type': 'application/json;charset=utf-8' // 默认值 
+        },
+        success(res) {
+          console.log(res,"456789")
+          const time = new Date(res.data.result.createTime);
+          console.log(time, "jgfghjk")
+          that.setData({
+            news: res.data.result,
+            // createTime: formatTime(time)
           })
         }
       })
-    }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
--- pages/log/log.js
+++ pages/log/log.js
@@ -1,101 +1,54 @@
-// pages/log/log.js
+//index.js
+//获取应用实例
+const app = getApp()
 
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
+    motto: 'hello',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    const that = this;
-    wx.request({
-      url: 'http://139.224.25.165:8066/api/getPictureList',
-      data: {
-        pageNum: 1,
-        pageSize: 6
-      },
-      header: {
-        'content-type': 'application/json;charset=utf-8' // 默认值 
-      },
-      success(res) {
-        that.setData({
-          banners: res.result
-        })
-        console.log(res, "67ffty")
-      }
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
-
-  // banners: function () {
-  //   wx.request({
-  //     url: 'http://139.224.25.165:8066/api/getPictureList',
-  //     data: {
-  //       pageNum: 1,
-  //       pageSize: 6
-  //     },
-  //     header: {
-  //       'content-type': 'application/json;charset=utf-8' // 默认值 
-  //     },
-  //     success(res) {
-  //       this.setData({
-  //         banners: res.result.list
-  //       })
-  //       console.log(res, "67ffty")
-  //     }
-  //   })
-  // },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
 })
